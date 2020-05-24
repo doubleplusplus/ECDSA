f923d204296aada7a529e369fce670b4456e1a54
Jamie <jdeng31@ucsc.edu>
2020-05-24 14:15:07 -0700

    sqrt mod prime

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ECDSA
  234hw8
 
-1. sqrt_mod algorithm adapted from [Tonelli-Shanks algorithm implementation of prime modular square root](https://codereview.stackexchange.com/questions/43210/tonelli-shanks-algorithm-implementation-of-prime-modular-square-root)
+1. sqrt_mod prime algorithm adapted from [Tonelli-Shanks algorithm](https://rosettacode.org/wiki/Tonelli-Shanks_algorithm#Python)
--- ecdsa.py
+++ ecdsa.py
@@ -1,79 +1,44 @@
+def legendre(a, p):
+    return pow(a, (p - 1) // 2, p)
 
-def legendre_symbol(a, p):
-    """
-    Legendre symbol
-    Define if a is a quadratic residue modulo odd prime
-    http://en.wikipedia.org/wiki/Legendre_symbol
-    """
-    ls = pow(a, int((p - 1)/2), p)
-    if ls == p - 1:
-        return -1
-    return ls
 
-def sqrt_mod(a, p):
-    """
-    Square root modulo prime number
-    Solve the equation
-        x^2 = a mod p
-    and return list of x solution
-    http://en.wikipedia.org/wiki/Tonelli-Shanks_algorithm
-    """
-    a %= p
-
-    # Simple case
-    if a == 0:
-        return [0]
-    if p == 2:
-        return [a]
-
-    # Check solution existence on odd prime
-    if legendre_symbol(a, p) != 1:
-        return []
-
-    # Simple case
-    if p % 4 == 3:
-        x = pow(a, int((p + 1)/4), p)
-        return [x, p-x]
-
-    # Factor p-1 on the form q * 2^s (with Q odd)
-    q, s = p - 1, 0
+def sqrt_mod(n, p):
+    assert legendre(n, p) == 1, "not a square (mod p)"
+    q = p - 1
+    s = 0
     while q % 2 == 0:
-        s += 1
         q //= 2
-
-    # Select a z which is a quadratic non resudue modulo p
-    z = 1
-    while legendre_symbol(z, p) != -1:
-        z += 1
+        s += 1
+    if s == 1:
+        return pow(n, (p + 1) // 4, p)
+    for z in range(2, p):
+        if p - 1 == legendre(z, p):
+            break
     c = pow(z, q, p)
-
-    # Search for a solution
-    x = pow(a, int((q + 1)/2), p)
-    t = pow(a, q, p)
+    r = pow(n, (q + 1) // 2, p)
+    t = pow(n, q, p)
     m = s
-    while t != 1:
-        # Find the lowest i such that t^(2^i) = 1
-        i, e = 0, 2
+    t2 = 0
+    while (t - 1) % p != 0:
+        t2 = (t * t) % p
         for i in range(1, m):
-            if pow(t, e, p) == 1:
+            if (t2 - 1) % p == 0:
                 break
-            e *= 2
-
-        # Update next value to iterate
-        b = pow(c, 2**(m - i - 1), p)
-        x = (x * b) % p
-        t = (t * b * b) % p
+            t2 = (t2 * t2) % p
+        b = pow(c, 1 << (m - i - 1), p)
+        r = (r * b) % p
         c = (b * b) % p
+        t = (t * c) % p
         m = i
-
-    return [x, p-x]
+    return r, p-r
 
 
 def q1():
     a = 5
     p = 407426149192123516349916653686361706730690466158799531798401
-    print(sqrt_mod(a, p))
-    print(pow(51, 2, 113) == 2)
+    r = sqrt_mod(a, p)
+    print(r)
+    print(pow(r[0], 2, p) == a, pow(r[1], 2, p) == a)
 
 
 q1()
